17b2e798fa409772788b0996dcff93787e57b611
Deng Haozhi <haozhi_deng@126.com>
2023-06-13 09:28:47 +0800

    endelss 0.0.3 Add modules! DDD rebuild! Deng Haozhi Commit!

---
+++ pluto/src/main/java/org/endless/pluto/PlutoApplication.java
@@ -0,0 +1,18 @@
+package org.endless.pluto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 冥王星应用程序
+ *
+ * @author Deng Haozhi
+ * @date 2023/06/12
+ */
+@SpringBootApplication
+public class PlutoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PlutoApplication.class, args);
+  }
+}
---
+++ pluto/src/test/java/org/endless/pluto/PlutoApplicationTests.java
@@ -0,0 +1,11 @@
+package org.endless.pluto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlutoApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
